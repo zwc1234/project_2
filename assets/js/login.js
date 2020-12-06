9137740d3717021cf33146d744d51054e5681e69
zwc <zwc@123.cn>
2020-12-06 10:59:18 +0800

    表单验证1

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,5 +6,12 @@ $( function () {
   $( '.reg-form a' ).on( 'click', function () {
     $( '.login-form' ).show()
     $('.reg-form').hide()
+  } )
+  var form=layui.form
+  form.verify( {
+    pwd: [
+      /^[\S]{6,12}$/
+      ,'密码必须6到12位，且不能出现空格'
+    ] 
   })
 })
