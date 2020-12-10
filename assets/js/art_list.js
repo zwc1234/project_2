ce751b895cf8e3303c7b329ea063de317c4617f7
zwc <zwc@123.cn>
2020-12-10 19:55:46 +0800

    大事件完成

--- assets/js/art_list.js
+++ assets/js/art_list.js
@@ -1,5 +1,6 @@
 $( function () {
-  let layer = layui.layer
+  let layer=layui.layer
+  let form=layui.form
   let q={
     pagenum: 1,
     pagesize: 2,
@@ -14,30 +15,123 @@ $( function () {
       data: q,
       success: function ( res ) {
         if ( res.status!==0 ) {
-          return layer.msg('获取失败')
+          return layer.msg( '获取失败' )
         }
         layer.msg( '获取成功' )
-        console.log(res.data);
+
         let htmlStr=template( 'tpl-table', res )
-       
-        $('tbody').html(htmlStr)
+
+        $( 'tbody' ).html( htmlStr )
+        renderPage( res.total )
       }
-    })
+    } )
   }
 
-  template.defaults.imports.dateFormat=function (date) {
+  template.defaults.imports.dateFormat=function ( date ) {
     const dt=new Date( date )
-    console.log(dt);
-    let y=padZore(dt.getFullYear())
-    let m=padZore(dt.getMonth()+1)
-    let d=padZore(dt.getDate())
-    let hh=padZore(dt.getHours())
-    let mm=padZore(dt.getMinutes())
-    let ss=padZore(dt.getSeconds())
-    return y + '-' + m +'-' + d + '  ' + hh + ':' + mm + ':' + ss
+    let y=padZore( dt.getFullYear() )
+    let m=padZore( dt.getMonth()+1 )
+    let d=padZore( dt.getDate() )
+    let hh=padZore( dt.getHours() )
+    let mm=padZore( dt.getMinutes() )
+    let ss=padZore( dt.getSeconds() )
+    return y+'-'+m+'-'+d+'  '+hh+':'+mm+':'+ss
   }
 
-  function padZore(d) {
-   return d > 9 ? d : '0' +d
+  function padZore( d ) {
+    return d>9? d:'0'+d
   }
+
+  function initCate() {
+    $.ajax( {
+      method: 'GET',
+      url: '/my/article/cates',
+      success: function ( res ) {
+
+        if ( res.status!==0 ) {
+          return layer.msg( '获取失败' )
+        }
+        let htmlStr=template( 'tpl-cate', res )
+        $( '[name="cate_Id"]' ).html( htmlStr )
+        form.render()
+      }
+    } )
+  }
+  initCate()
+
+  $( '#form-search' ).submit( function ( e ) {
+    e.preventDefault()
+    let cate_Id=$( '[name="cate_Id"]' ).val()
+    let state=$( '[name="state"]' ).val()
+    q.cate_id=cate_Id
+    q.state=state
+    initTable()
+  } )
+  //分页
+  var laypage=layui.laypage;
+  function renderPage( total ) {
+    laypage.render( {
+      elem: 'pageBox',
+      count: total,
+      limit: q.pagesize,
+      curr: q.pagenum,
+      limits:[2,3,5,10],
+      layout:['count','limit','prev', 'page', 'next','skip'],
+      jump: function ( obj,first ) {
+        q.pagenum=obj.curr
+        q.pagesize = obj.limit
+        if ( !first ) {
+          initTable()
+        }
+      }
+    } )
+  }
+  
+
+  $( 'tbody' ).on( 'click', '.btn-delete', function () {
+    let id=$( this ).attr( 'data-id' )
+    let len=$( '.btn-delete' ).length
+
+    layer.confirm( '确认删除?', { icon: 3, title: '提示' }, function ( index ) {
+      $.ajax( {
+        method: 'GET',
+        url: '/my/article/delete/'+id,
+        success: function (res) {
+          if ( res.status!==0 ) {
+            return layer.msg(res.message)
+          }
+          layer.msg( res.message )
+          if ( len ===1 ) {
+            q.pagenum=q.pagenum===1? 1:q.pagenum-1;
+          }
+
+          initTable()
+        }
+      })
+     
+      layer.close(index);
+    } );
+   
+  } )
+  
+  $( 'body' ).on( 'click', '.btn-edit',function () {
+    let id=$( this ).attr( 'data-id' )
+    $.ajax( {
+      method: 'GET',
+      url: '/my/article/'+id,
+      success: function ( res ) {
+
+        if ( res.status!==0 ) {
+          return layer.msg(res.message)
+        }
+
+        localStorage.setItem('data',JSON.stringify(res.data))
+        location.href = '/arcitle/art_pub.html'
+      
+
+        }
+    } )
+    
+    
+  })
 } )
---
+++ assets/js/art_pub.js
@@ -0,0 +1,100 @@
+$( function () {
+  let layer = layui.layer
+  let form = layui.form
+  function initCate() {
+    $.ajax( {
+      method: 'GET',
+      url: '/my/article/cates',
+      success: function ( res ) {
+        if ( res.status!==0 ) {
+          return layer.msg(res.message)
+        }
+        layer.msg( res.message )
+        let htmlStr =template('tpl-cate', res)
+        $( '[name=cate_id]' ).html( htmlStr )
+        form.render()
+      }
+    })
+  }
+  initCate()
+
+  initEditor()
+    // 1. 初始化图片裁剪器
+    var $image = $('#image')
+  
+    // 2. 裁剪选项
+    var options = {
+      aspectRatio: 400 / 280,
+      preview: '.img-preview'
+    }
+    
+    // 3. 初始化裁剪区域
+  $image.cropper( options )
+  $( '#btnimg' ).click( function () {
+    $('#coverfile').click()
+  } )
+  $( '#coverfile' ).change( function (e) {
+    let files=e.target.files
+    if ( files.length===0 ) {
+      return
+    }
+     let file=files[ 0 ]
+  
+    let  newImgURL=URL.createObjectURL( file )
+    $image
+   .cropper('destroy')      // 销毁旧的裁剪区域
+   .attr('src', newImgURL)  // 重新设置图片路径
+   .cropper(options)        // 重新初始化裁剪区域
+  } )
+  let art_state='已发布'
+  $( '#btnsave' ).click( function () {
+    art_state = '草稿'
+  } )
+  $( '#form-pub' ).submit( function (e) {
+    e.preventDefault()
+    let fd=new FormData( this )
+    fd.append( 'state', art_state )
+    $image
+  .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+    width: 400,
+    height: 280
+  })
+  .toBlob(function(blob) {       // 将 Canvas 画布上的内容，转化为文件对象
+    // 得到文件对象后，进行后续的操作
+    fd.append( 'cover_img', blob )
+    console.log(fd);
+    publishArtList(fd)
+  })
+  } )
+
+
+  function publishArtList(fd) {
+    $.ajax( {
+      method: 'POST',
+      url: '/my/article/add',
+      data: fd,
+      processData: false,
+          contentType: false,
+      success: function (res) {
+        if ( res.status!==0 ) {
+          return layer.msg(res.message)
+        }
+        layer.msg( res.message )
+        location.href= '/arcitle/art_list.html'
+      }
+    })
+  }
+  function render() {
+    let data=JSON.parse( localStorage.getItem( 'data' ) )
+    console.log( data );
+    let nme=''
+    $.get( '/my/article/cates', function ( res ) {
+      console.log( res );
+      form.val('form-pub',data)
+    } )
+    
+   
+  }
+  render()
+  
+})
